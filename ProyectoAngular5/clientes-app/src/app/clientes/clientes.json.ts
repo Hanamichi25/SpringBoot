6b8fb321ed00650c191843c2731fe9cc7631d659
drmartin@ownk.co <drmartin@ownk.co>
2018-03-25 18:54:13 -0500

    Creacion de pagina Clientes

---
+++ ProyectoAngular5/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[]=[
+  {id:1, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:2, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:3, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:4, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:5, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:6, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:7, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:8, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:9, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:10, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'},
+  {id:11, nombre: 'Daniel', apellido: 'Martin', email:'drmartin@gmail.com', createAt:'2018-03-25'}
+];
